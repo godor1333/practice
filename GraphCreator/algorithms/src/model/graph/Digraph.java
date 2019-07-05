1dce5fac6e1e44bbfd00b4ea1ae9d4f0857dd0cb
AngelinaKalyaeva <a.kalyaeva06@mail.ru>
2019-07-05 23:37:21 +0300

    feature: Constructor for digraph was added.

--- GraphCreator/algorithms/src/model/graph/Digraph.java
+++ GraphCreator/algorithms/src/model/graph/Digraph.java
@@ -1,10 +1,18 @@
 package model.graph;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Digraph {
     private int vertexCount;
     private List<Set<DirectedEdge>> edges = new ArrayList<>();
+
+    public Digraph(int vertexCount) {
+        this.vertexCount = vertexCount;
+        for (int vertexNumber = 0; vertexNumber < vertexCount; vertexNumber++) {
+            this.edges.add(new LinkedHashSet<>());
+        }
+    }
 }

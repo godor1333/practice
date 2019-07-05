e7e5f6d651a3554334d311c694141a4824f6d089
AngelinaKalyaeva <a.kalyaeva06@mail.ru>
2019-07-05 23:36:04 +0300

    feature: Getters was added for directed edge.

--- GraphCreator/algorithms/src/model/graph/Digraph.java
+++ GraphCreator/algorithms/src/model/graph/Digraph.java
@@ -1,4 +1,10 @@
 package model.graph;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class Digraph {
+    private int vertexCount;
+    private List<Set<DirectedEdge>> edges = new ArrayList<>();
 }

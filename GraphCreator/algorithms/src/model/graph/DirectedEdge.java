c014cc8b2cda34de26b6c244b2ef44e0f9d6ef0f
AngelinaKalyaeva <a.kalyaeva06@mail.ru>
2019-07-05 23:25:33 +0300

    feature: Variables was added to directed edge.

--- GraphCreator/algorithms/src/model/graph/DirectedEdge.java
+++ GraphCreator/algorithms/src/model/graph/DirectedEdge.java
@@ -1,4 +1,13 @@
 package model.graph;
 
 public class DirectedEdge {
+    private final int source;
+    private final int target;
+    private final double weight;
+
+    public DirectedEdge(int source, int target, double weight) {
+        this.source = source;
+        this.target = target;
+        this.weight = weight;
+    }
 }

18876b982df222962a560b41b576783dd5b500a8
AngelinaKalyaeva <a.kalyaeva06@mail.ru>
2019-07-05 23:47:28 +0300

    fix: Constructor was simplified by separation.

--- GraphCreator/algorithms/src/model/graph/WeightedDigraph.java
+++ GraphCreator/algorithms/src/model/graph/WeightedDigraph.java
@@ -7,13 +7,22 @@ public class WeightedDigraph {
     private Map<String, Integer> vertexNameOfNumber;
 
     public WeightedDigraph(String stream, String separator) {
+        final List<List<String>> graphStringRepresentation = prepareGraphStringRepresentation(stream, separator);
+        final int graphSize = graphStringRepresentation.stream().map(List::size).reduce(Integer::sum).orElse(0);
+        buildGraphFromStringRepresentation(graphStringRepresentation, graphSize);
+    }
+
+    private List<List<String>> prepareGraphStringRepresentation(String stream, String separator) {
         final List<List<String>> graphStringRepresentation = new ArrayList<>();
         final Scanner scanner = new Scanner(stream);
         while (scanner.hasNext()) {
             List<String> stringGraph = Arrays.asList(scanner.nextLine().split(String.format("[\\%s]", separator)));
             graphStringRepresentation.add(stringGraph);
         }
-        final int graphSize = graphStringRepresentation.stream().map(List::size).reduce(Integer::sum).orElse(0);
+        return graphStringRepresentation;
+    }
+
+    private void buildGraphFromStringRepresentation(List<List<String>> graphStringRepresentation, int graphSize) {
         graph = new Digraph(graphSize);
         vertexNameOfNumber = new HashMap<>();
         for (List<String> graphLine : graphStringRepresentation) {

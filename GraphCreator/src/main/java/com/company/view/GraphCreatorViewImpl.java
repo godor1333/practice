7d509fc8f324599f30c08296554bfc7114544266
godor1333 <31541802+godor1333@users.noreply.github.com>
2019-07-05 22:21:40 +0300

    gui-change
    add toolBar and text Area (logs)

--- GraphCreator/src/main/java/com/company/view/GraphCreatorViewImpl.java
+++ GraphCreator/src/main/java/com/company/view/GraphCreatorViewImpl.java
@@ -14,11 +14,12 @@ import java.io.File;
 
 import static com.company.Constants.Size.*;
 
-public class GraphCreatorViewImpl extends JFrame implements GraphCreatorView{
+public class GraphCreatorViewImpl extends JFrame implements GraphCreatorView {
 
     private final GraphCreatorController controller;
     private final GraphCreatorModel model;
     private ToolBar toolBar;
+    private JTextArea textArea;
 
     public GraphCreatorViewImpl(GraphCreatorController controller, GraphCreatorModel model) {
         super("Graph creator");
@@ -30,16 +31,23 @@ public class GraphCreatorViewImpl extends JFrame implements GraphCreatorView{
     }
 
     private void initGUI() {
-        setSize(Constants.Size.WIDTH, Constants.Size.HEIGHT);
+        Dimension sSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(sSize.width, sSize.height);
         setResizable(false);
         setLocationRelativeTo(null);
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout(INTEND, INTEND));
         panel.setBorder(new EmptyBorder(INTEND, INTEND, INTEND, INTEND));
         toolBar = new ToolBar(controller);
-        toolBar.setPreferredSize(new Dimension(Constants.Size.WIDTH, (int) (Constants.Size.HEIGHT * 0.1)));
+        toolBar.setPreferredSize(new Dimension((int) (sSize.width * 0.95), (int) (sSize.height * 0.13)));
         Creator creator = new Creator(controller, model);
-        creator.setPreferredSize(new Dimension(Constants.Size.WIDTH, (int) (Constants.Size.HEIGHT * 0.85)));
+        creator.setPreferredSize(new Dimension((int) (sSize.width * 0.7), (int) (sSize.height * 0.75)));
+        textArea = new JTextArea(10, 50);
+        textArea.setEnabled(false);
+        textArea.setDisabledTextColor(Color.BLACK);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setPreferredSize(new Dimension((int) (sSize.width * 0.25), (int) (sSize.height * 0.75)));
+        panel.add(scrollPane, BorderLayout.WEST);
         panel.add(toolBar, BorderLayout.NORTH);
         panel.add(creator, BorderLayout.CENTER);
         getContentPane().add(panel);
@@ -48,8 +56,23 @@ public class GraphCreatorViewImpl extends JFrame implements GraphCreatorView{
     }
 
     @Override
-    public void setEnabledControlStepButton(boolean show) {
-        toolBar.setEnabledControlStepButton(show);
+    public void setEnabledStartButton(boolean show) {
+        toolBar.setEnabledStartButton(show);
+    }
+
+    @Override
+    public void setEnabledFinishButton(boolean show) {
+        toolBar.setEnabledFinishButton(show);
+    }
+
+    @Override
+    public void setEnabledNextButton(boolean show) {
+        toolBar.setEnabledNextButton(show);
+    }
+
+    @Override
+    public void setEnabledBackButton(boolean show) {
+        toolBar.setEnabledBackButton(show);
     }
 
     @Override
@@ -58,8 +81,13 @@ public class GraphCreatorViewImpl extends JFrame implements GraphCreatorView{
     }
 
     @Override
+    public void setLog(String message) {
+        textArea.append(message + "\n");
+    }
+
+    @Override
     public void showErrorDialog(String title, String message) {
-        JOptionPane.showMessageDialog(this, message,title,JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(this, message, title, JOptionPane.ERROR_MESSAGE);
     }
 
     @Override

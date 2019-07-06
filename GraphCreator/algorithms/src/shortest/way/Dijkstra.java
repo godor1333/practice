ec5aa386a0725aad9c7b14c258d9c6cc4b27bbe6
AngelinaKalyaeva <a.kalyaeva06@mail.ru>
2019-07-06 20:41:26 +0300

    feature: Dijkstra draft was added.

---
+++ GraphCreator/algorithms/src/shortest/way/Dijkstra.java
@@ -0,0 +1,13 @@
+package shortest.way;
+
+import model.graph.DirectedEdge;
+
+import java.util.PriorityQueue;
+
+public class Dijkstra {
+    private DirectedEdge[] edgeTo;
+    private double[] distTo;
+    private boolean[] visitedVertex;
+    private PriorityQueue<Double> priorityQueue;
+
+}
